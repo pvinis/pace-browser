0e4f31a97f6717e3f44087932e976934ac6b5d98
Pavlos Vinieratos <pvinis@gmail.com>
2025-03-03 18:56:33 +0200

    url bar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import "../global.css";
-
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function Index() {
+  const [url, setUrl] = useState("https://google.com");
+
   return (
     <View className="flex-1 bg-red-500 py-safe">
       <WebView
+        className="flex-1"
         source={{ uri: "https://google.com" }}
-        // originWhitelist={["*"]}
-        // source={{ html: "<h1><center>Hello world</center></h1>" }}
+        pullToRefreshEnabled
       />
-      <Text>Edit app/index.tsx to edi this screen.</Text>
-      <View className="bg-blue-500">
-        <Text>Hello</Text>
+      <View className="bg-blue-500 absolute bottom-safe left-0 right-0">
+        <View className="bg-blue-300 py-2 px-4 my-2 mx-4 rounded-lg">
+          <TextInput className="" value={url} onChangeText={setUrl} />
+        </View>
       </View>
     </View>
   );
